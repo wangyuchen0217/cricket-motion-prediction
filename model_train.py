b45a8fad38f86fdaef4e5ab6613e1fdaf4db2ba9
Yuhen-W <wangyuchen0217@outlook.com>
2023-04-04 15:05:41 +0900

    commit

--- model_train.py
+++ model_train.py
@@ -62,7 +62,7 @@ if __name__ == '__main__':
             subject_number = str(i)
         # get dataset for train and test seperately
         # 1 test set for each cricket (c16, c17, c18, c19, c20, c21) 
-        with open("trail_details.json", "r") as f:
+        with open("/home/yuchen/Crickets_Walking_Motion_Prediction/trail_details.json", "r") as f:  
             trail_details = json.load(f)
             cricket_number =  trail_details[f"T{subject_number}"]["cricket_number"]
             video_number = trail_details[f"T{subject_number}"]["video_number"]
