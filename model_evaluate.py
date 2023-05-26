ee223f3a60e1f37eb60d24ba1d56521c6e9297ec
Yuhen-W <wangyuchen0217@outlook.com>
2023-05-26 17:11:26 +0900

    commit

--- model_evaluate.py
+++ model_evaluate.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     window_size = 100
     time_step = 1
     ###### choose the output mode ###### 
-    out_content = "Vel" # ["Vel","Direction"]
+    out_content = "Direction" # ["Vel","Direction"]
     test_trails = ["c16","c17","c18","c19","c20","c21"]
     ###### set the model ######
     model_type = "arx" # ["lstm","hlstm","arx","trans"]
