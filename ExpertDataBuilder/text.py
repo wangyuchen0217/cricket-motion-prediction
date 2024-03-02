242ad325eed77d0b1192b3d1546685f85924d4cb
wangyuchen0217 <wangyuchen0217@outlook.com>
2024-03-02 11:16:52 +0900

    commit

--- ExpertDataBuilder/text.py
+++
@@ -1,9 +0,0 @@
-import os
-import json
-import math
-import numpy as np
-import pandas as pd
-
-# Get the upper level directory
-dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-print(dir_path)
